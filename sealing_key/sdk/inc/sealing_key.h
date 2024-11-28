e025b79d82464af4c4fd245d80a8d35fd33fbbe3
chenzheng <chenzheng71@huawei.com>
2024-11-28 04:05:11 +0000

    change variable to avoid ambiguity

--- sealing_key/sdk/inc/sealing_key.h
+++ sealing_key/sdk/inc/sealing_key.h
@@ -16,7 +16,7 @@
 #include <stdint.h>
 
 #define SEALING_KEY_LEN 32
-#define SEALING_SALT_LEN 64
+#define SEALING_USER_PARAM_LEN 64
 
 typedef enum {
     SEALING_HMAC_SHA256
@@ -25,16 +25,18 @@ typedef enum {
 /**
  * @brief   Get a sealing key from TMM with specified derivation parameters by PBKDF2 HUK derived
  *
- * @param   alg         [IN]  The HMAC algorithm used in derive sealing key
- * @param   salt        [IN]  A user param used in huk derivation, length should be 64 byte.
- *                            This param is optional, set it to NULL to derived without user param.
- * @param   salt_len    [IN]  Length of the user param in byte, should be 64. or set to 0 when not specifying user param.
- * @param   sealing_key [OUT] Addr of the derived sealing key
- * @param   key_len     [IN]  Length of the sealing_key buff, should not less than 32
+ * @param   alg             [IN]  The HMAC algorithm used in derive sealing key
+ * @param   user_param      [IN]  A user param used in huk derivation, length should be 64 byte.
+ *                                This param is optional, set it to NULL to derived without user param.
+ * @param   user_param_len  [IN]  Length of the user param in byte, should be 64, or set to 0
+ *                                when not specifying user param.
+ * @param   sealing_key     [OUT] Addr of the derived sealing key
+ * @param   key_len         [IN]  Length of the sealing_key buff, should not less than 32
  *
  * @return  0: successfully get the derived key
  *          -1: failed
 */
-int get_sealing_key(SEALING_KEY_ALG alg, uint8_t* salt, uint32_t salt_len, uint8_t* sealing_key, uint32_t key_len);
+int get_sealing_key(SEALING_KEY_ALG alg, uint8_t* user_param, uint32_t user_param_len, uint8_t* sealing_key,
+                    uint32_t key_len);
 
 #endif
