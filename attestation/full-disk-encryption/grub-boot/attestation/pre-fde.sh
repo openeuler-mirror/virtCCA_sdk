ebefeca8f3e13378cb4b1ec3556bc13072369739
gongchangsui <gongchangsui@outlook.com>
2025-03-02 21:08:30 -0500

    support rats-tls

--- attestation/full-disk-encryption/grub-boot/attestation/pre-fde.sh
+++ attestation/full-disk-encryption/grub-boot/attestation/pre-fde.sh
@@ -5,15 +5,9 @@ set -e
 THIS_DIR=$(dirname "$(readlink -f "$0")")
 ATTEST_SDK_DIR=${THIS_DIR}/../../../../attestation
 FDE_DIR=${THIS_DIR}/../../../full-disk-encryption/grub-boot
-ATTEST_DIR=${FDE_DIR}/attestation
 
-ROOT_CERT_FILE=${ATTEST_DIR}/root_cert.pem
 ROOT_CERT_URL="https://download.huawei.com/dl/download.do?actionFlag=download&nid=PKI1000000002&partNo=3001&mid=SUP_PKI"
-SUB_CERT_FILE=${ATTEST_DIR}/sub_cert.pem
 SUB_CERT_URL="https://download.huawei.com/dl/download.do?actionFlag=download&nid=PKI1000000040&partNo=3001&mid=SUP_PKI"
-ROOTFS_KEY_FILE=${ATTEST_DIR}/rootfs_key.bin
-ATTEST_CLIENT=${ATTEST_DIR}/client
-ATTEST_SERVER=${ATTEST_DIR}/server
 
 info() {
     echo -e "\e[1;33m$*\e[0;0m"
@@ -47,6 +41,56 @@ check_tools() {
     [[ $is_missing != true ]] || { error "Please install missing tools"; }
 }
 
+# Display Usage information
+usage() {
+    cat << EOM
+Usage: $(basename "$0") [OPTION]...
+Required
+  -a <attest case>              Specify local verifier for attestation, 
+                                please input samples or rats-tls
+EOM
+}
+
+process_args() {
+    while getopts "a:h" option; do
+        case "$option" in
+        a) ATTEST_CASE=$OPTARG;;
+        h)
+            usage
+            exit 0
+            ;;
+        *)
+            echo "Invalid option '-$OPTARG'"
+            usage
+            exit 1
+            ;;
+        esac
+    done
+
+    if [ ${ATTEST_CASE} = "samples" ] || [ ${ATTEST_CASE} = "rats-tls" ]; then
+        ATTEST_DIR=${FDE_DIR}/attestation/${ATTEST_CASE}
+        mkdir -p ${ATTEST_DIR}
+        ATTEST_CLIENT=${ATTEST_DIR}/virtcca-client
+        ATTEST_SERVER=${ATTEST_DIR}/virtcca-server
+        ROOT_CERT_FILE=${ATTEST_DIR}/root_cert.pem
+        SUB_CERT_FILE=${ATTEST_DIR}/sub_cert.pem
+        ROOTFS_KEY_FILE=${ATTEST_DIR}/rootfs_key.bin
+    else
+        error "Please specify samples or rats-tls for attestation"
+    fi
+
+    ok "=================================================================="
+    ok "Attest path: ${ATTEST_DIR}"
+    ok "Attest client: ${ATTEST_CLIENT}"
+    ok "Attest server: ${ATTEST_SERVER}"
+    ok "Root cert: ${ROOT_CERT_FILE}"
+    ok "Sub cert: ${SUB_CERT_FILE}"
+    ok "Rootfs key: ${ROOTFS_KEY_FILE}"
+    ok "=================================================================="
+
+     check_tools yum install tar cmake make git gcc
+}
+
 build_sdk() {
     info "Build attestation sdk ..."
     cd ${ATTEST_SDK_DIR}/sdk
@@ -61,9 +105,46 @@ build_samples() {
     cd ${ATTEST_SDK_DIR}/samples
     cmake -S . -B build
     cmake --build build
+    # Copy samples to ${ATTEST_DIR}
     cp ${ATTEST_SDK_DIR}/samples/build/client ${ATTEST_CLIENT}
     cp ${ATTEST_SDK_DIR}/samples/build/server ${ATTEST_SERVER}
     cd ${ATTEST_DIR}
+}
+
+build_libcbor() {
+    info "Build libcbor for rats-tls ..."
+    cd ${ATTEST_SDK_DIR}/rats-tls
+    if [ -d "libcbor/.git" ]; then
+        info "rats-tls git repo already exists"
+    else
+        git clone https://github.com/PJK/libcbor.git
+    fi
+    cd libcbor
+    cmake -S . -B build
+    cd build
+    make
+    make install
+    cd ${ATTEST_DIR}
+}
+
+build_rats_tls() {
+    info "Build attestation rats-tls ..."
+    cd ${ATTEST_SDK_DIR}/rats-tls
+    if [ -d "rats-tls/.git" ]; then
+        info "rats-tls git repo already exists"
+    else
+        git clone https://github.com/inclavare-containers/rats-tls.git
+    fi
+    cd rats-tls
+    git reset --hard 40f7b78403d75d13b1a372c769b2600f62b02692
+    git apply ../*.patch
+    bash build.sh -s -r
+    # Copy rats-tls to ${ATTEST_DIR} and install envs
+    cd ${ATTEST_DIR}
+    cp ${ATTEST_SDK_DIR}/rats-tls/rats-tls/bin/rats-tls.tar.gz ${ATTEST_DIR}
+    tar -zxf rats-tls.tar.gz
+    cp -rf lib/rats-tls /usr/lib/
+    export LD_LIBRARY_PATH=/usr/lib/rats-tls:$LD_LIBRARY_PATH
 }
 
 download_certfile() {
@@ -87,13 +168,25 @@ generate_rootfskey() {
     fi
 }
 
+ok "Check which attestation cases will be built ..."
+
+process_args "$@"
+
 ok "Prepare attestation envs, include apps, certs ..."
 
 if [[ ! -f ${ATTEST_CLIENT} || ! -f ${ATTEST_SERVER} ]] ; then
     info "Attestation apps do not exist, re-build ..."
-    check_tools yum install tar cmake make git gcc
     build_sdk
-    build_samples
+    if [ ${ATTEST_CASE} = "samples" ]; then
+        build_samples
+    else
+        if [ ! -f /usr/local/lib64/libcbor.a ]; then
+            build_libcbor
+        fi
+        build_rats_tls
+    fi
+else
+    warn "Attestation apps already exist, if re-build please delete existing apps"
 fi
 
 download_certfile
--- attestation/full-disk-encryption/grub-boot/initramfs/rats-tls/fde-agent.sh
+++ attestation/full-disk-encryption/grub-boot/initramfs/rats-tls/fde-agent.sh
@@ -6,9 +6,11 @@ IP_ADDR=$(ip addr show eth0 | sed -n '/inet /s/.*inet \([0-9.]\+\).*/\1/p')
 echo "fde: Running attestation in pre-mount phase..." > /dev/console
 echo "fde: CVM IP Address: ${IP_ADDR} ..." > /dev/console
 echo "fde: Attestation server port: 7220 ..." > /dev/console
-/usr/bin/server -i ${IP_ADDR} -p 7220 -k 
+export LD_LIBRARY_PATH=/usr/lib/rats-tls:$LD_LIBRARY_PATH
+/usr/bin/virtcca-server -i ${IP_ADDR} -p 7220 
 sleep 1
 
 echo "fde: Running rootfs decryption in initramfs..." > /dev/console
 cryptsetup open /dev/vda2 encroot --key-file /root/rootfs_key.bin
+rm /root/rootfs_key.bin
 
